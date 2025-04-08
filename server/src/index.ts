c048821a49ab9dbe5ec0aee9a0015433bfea0636
mateohysa <hysamateo@gmail.com>
2025-04-08 20:07:59 +0200

    initial installations and backend setups are in place

---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// import routes
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("This is the home route");
+});
+
+// SERVER
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
